af01837d2e39566c7d16dac817e97b8dbccf3b47
mizukanaAlt <rina301087@gmail.com>
2026-05-28 18:59:17 +0000

    techpack: xiaomi-msm8956: migrate all header to msm-eldarion Header

--- leds/leds-aw2013.c
+++ leds/leds-aw2013.c
@@ -20,7 +20,7 @@
 #include <linux/mutex.h>
 #include <linux/slab.h>
 #include <linux/regulator/consumer.h>
-#include <xiaomi-titanium/leds_aw2013.h>
+#include <msm-eldarion/leds_aw2013.h>
 
 /* register address */
 #define AW_REG_RESET			0x00
